bdb93c819c92c6fc305cca4437fd703532b01849
jessicahaara <jessica.haara@iths.se>
2021-04-19 14:56:11 +0200

    middleware

--- server.js
+++ server.js
@@ -4,15 +4,26 @@ const app = express();
 const cors = require('cors');
 const path = require('path');
 
-const hamsters = require('./routes/hamsters.js');
-
 const PORT = 1111
 
-app.use('/hamsters', hamsters);
+const hamsters = require('./routes/hamsters.js');
+
+
+app.use((req, res, next) => {
+    console.log(`${req.method} ${req.url} `, req.params);
+    next()
+})
+
+app.use( express. json() )
+app.use( cors() )
+
 
 app.get('/', (req, res) => {
     res.send('Hello from server')
 })
+
+app.use('/hamsters', hamsters);
+
 
 app.listen(PORT, (req,res) => {
     console.log(`Server started at PORT: ${PORT}`);
