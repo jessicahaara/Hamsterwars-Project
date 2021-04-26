df9ca0a4c31b5294ff93d55cfe0b248ac2803531
jessicahaara <jessica.haara@iths.se>
2021-04-26 10:18:18 +0200

    small changes to script.js

--- static/script.js
+++ static/script.js
@@ -2,9 +2,9 @@ const h1 = document.querySelector('h1')
 
 
 h1.addEventListener('mouseover', () => {
-        h1.innerHTML = 'Hej'
+        h1.innerHTML = 'Hej snällis'
 })
 
 h1.addEventListener('mouseout', () => {
-        h1.innerHTML = 'Fronend'
+        h1.innerHTML = 'Frontend'
 })
